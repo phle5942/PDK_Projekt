cda74f08b7ad1cb26442a8b42dbc3154aa9c1fcc
Marcus Lindqvist <marcus.lindqvist.0410@student.uu.se>
2026-02-16 16:08:15 +0100

    compare function done

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,31 @@
 type Movie = number;
-type Rating = number;
+// type Rating = number;
 type UserID = number;
 
 console.log("Hello, world!");
 
 type User = {
   id: number,
-  movie_list: Array<{ movie: number, rating: number }>
+  movie_list: Array<Movie>
+}
+
+function compare(user1: User, user2: User): Array<Movie> {
+  const u2_lst = user2.movie_list;
+  const u1_lst = user1.movie_list;
+
+  const return_array = Array<Movie>();
+
+  for (let i = 0; i < u2_lst.length; i++) {
+    let same = false;
+    for (let j = 0; j < u1_lst.length; j++) {
+      if (u2_lst[i] === u1_lst[j]) {
+        same = true;
+      }
+    }
+    if (!same) {
+      return_array.push(u2_lst[i]);
+    }
+  }
+
+  return return_array;
 }
