69bb57421b0ea23c5f36bb82fbc3dab67dc6c8f5
Marcus Lindqvist <marcus.lindqvist.0410@student.uu.se>
2026-02-20 16:59:31 +0100

    sim score och assign wieght

--- src/main.js
+++ src/main.js
@@ -7,11 +7,14 @@ var csv = require("csv-parser");
 function similarityScore(main_user, other_user) { return 1; }
 // Returns how much a specific users "vote" counts.
 function assign_weight(similarity, rating) { return 1; }
-// init hashtable
+// init hashtable for relevant users
 var userMovieTable = hash.ph_empty(610, hash.hash_id);
+/// init hashtable for movie and score
+var movieScoreTable = hash.ph_empty(100, hash.hash_id);
 var inputMovies = [1, 4, 7, 12, 54];
 hash.ph_insert(userMovieTable, 1, [{ movie: 2, rating: 3 }]);
-function getRelevantUsers(movies, callback) {
+///////////////////////////////////////////////////7
+function getRelevantUsers(movies, func) {
     // set boolean to keep track of relevant users
     var hasSeen = false;
     // keep track of current user, this assumes the file is sorted with regards to users
@@ -23,26 +26,28 @@ function getRelevantUsers(movies, callback) {
         var user = Number(row.userId);
         var movie = Number(row.movieId);
         var rating = Number(row.rating);
+        // when coming across new user, check if last user is relevant or not
         if (user !== currentUser && user !== -1) {
             if (hasSeen) {
                 hash.ph_insert(userMovieTable, currentUser, currentUserArray);
-                console.log(currentUser);
-                console.log(currentUserArray);
             }
             hasSeen = false;
             currentUserArray = [];
         }
+        // sets currentuser and pushes movies to temp array
         currentUser = user;
         currentUserArray.push({ movie: movie, rating: rating });
+        // sets true if user has seen one of the input movies
         if (movies.includes(movie)) {
             hasSeen = true;
         }
     })
         .on("end", function () {
+        // adds the last user
         if (hasSeen) {
             hash.ph_insert(userMovieTable, currentUser, currentUserArray);
         }
-        callback();
+        func();
     });
 }
 getRelevantUsers(inputMovies, function () {
@@ -50,3 +55,4 @@ getRelevantUsers(inputMovies, function () {
     list.for_each((function (key) { return console.log(hash.ph_lookup(userMovieTable, key)); }), lst);
 });
 // console.log(userMovieTable);
+console.log("hej");
--- src/main.ts
+++ src/main.ts
@@ -17,19 +17,37 @@ export type User = number;
 
 
 
-function similarityScore(main_user: User, other_user: User): number { return 1; }
+function similarityScore(input: Array<Movie>, movieArr: MovieArray): number { 
+  let relevantMoviesWatched = 0;
+  let totalRating = 0;
+
+  for (let i = 0; i < movieArr.length; i = i + 1) {
+    if (input.includes(movieArr[i].movie)) {
+      relevantMoviesWatched = relevantMoviesWatched + 1;
+      totalRating = totalRating + movieArr[i].rating;
+    }
+  }
+
+  return totalRating / relevantMoviesWatched; 
+}
 
 // Returns how much a specific users "vote" counts.
-function assign_weight(similarity: number, rating: number): number { return 1; }
+function assign_weight(similarity: number, rating: number): number {
+  return (rating - 2.5) * similarity;
+}
 
-// init hashtable
-const userMovieTable = hash.ph_empty<User, MovieArray>(610, hash.hash_id)
+// init hashtable for relevant users
+const userMovieTable = hash.ph_empty<User, MovieArray>(610, hash.hash_id);
 
+/// init hashtable for movie and score
+const movieScoreTable = hash.ph_empty<Movie, number>(100, hash.hash_id);
+
+// temproray inout movies for testing
 const inputMovies: Array<Movie> = [1, 4, 7, 12, 54];
 
-hash.ph_insert(userMovieTable, 1, [{movie: 2 ,rating: 3}]);
 
-function getRelevantUsers(movies: Array<Movie>, callback: () => void): void {
+///////////////////////////////////////////////////7
+function getRelevantUsers(movies: Array<Movie>, func: () => void): void {
   // set boolean to keep track of relevant users
   let hasSeen = false;
 
@@ -44,27 +62,30 @@ function getRelevantUsers(movies: Array<Movie>, callback: () => void): void {
       const movie: Movie = Number(row.movieId);
       const rating: Rating = Number(row.rating);
 
+      // when coming across new user, check if last user is relevant or not
       if (user !== currentUser && user !== -1) {
         if (hasSeen) {
           hash.ph_insert(userMovieTable, currentUser, currentUserArray);
-          console.log(currentUser);
-          console.log(currentUserArray);
         }
         hasSeen = false;
         currentUserArray = [];
       }
+
+      // sets currentuser and pushes movies to temp array
       currentUser = user;
       currentUserArray.push({ movie, rating })
 
+      // sets true if user has seen one of the input movies
       if (movies.includes(movie)) { hasSeen = true; }
        
 
     })
     .on("end", () => {
+      // adds the last user
       if(hasSeen) {
         hash.ph_insert(userMovieTable, currentUser, currentUserArray);
       }
-      callback();
+      func();
     })
 }
 
@@ -75,4 +96,4 @@ getRelevantUsers(inputMovies, () => {
 });
 // console.log(userMovieTable);
 
-
+console.log("hej");
