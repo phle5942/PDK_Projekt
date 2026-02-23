edafd5a5a5d34c6e551544b053a52a700cdcbaca
phle5942 <phoenix.lepp.5942@student.uu.se>
2026-02-23 17:13:26 +0100

    terminalinterface och till och från id till moviename fungerar

---
+++ src/interface.ts
@@ -0,0 +1,47 @@
+import readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+import { name_to_id } from "./name_to_id.ts";
+import {main} from "./main.ts"
+import { id_to_name } from "./id_to_movie_name.ts";
+const rl = readline.createInterface({ input, output });
+
+async function main_interface() {
+  console.log("Welcome, select 5 movies");
+
+  const movies: number[] = [];
+    let x = 0;
+    while(x < 5) {
+        const answer = await rl.question("Select : ");
+        const movie = await name_to_id(answer);
+        if(movie !== undefined) {
+            console.log(movie);
+            const yesorno = await rl.question(`Is this the right movie? (y/n)`);
+            if(yesorno === "y") {
+                x = x + 1;
+                movies.push(movie.movie_id);
+                continue;
+            }
+        }
+        else {
+            console.log("Movie not found, try harder")
+            continue;
+        }
+    }
+  
+    const recommended_movies = await main(movies);
+    let i = 0;
+    const r2 = readline.createInterface({ input, output });
+    while(recommended_movies.length > i) {
+        const final_movie = await id_to_name(recommended_movies[i][0]);
+        const answer = await r2.question(`Wadduya say about ${final_movie?.movie_title},  (y/n)`);
+        if(answer === "y") {
+            break;
+        } else {
+            i = i + 1;
+            continue;
+        }
+
+    }
+}
+
+main_interface();
---
+++ src/name_to_id.ts
@@ -0,0 +1,32 @@
+
+import fs from "fs"
+import csv from "csv-parser"
+export type Movie = number;
+type MovieResult = {movie_id : Movie, movie_title : string, movie_genres : string};
+//gets a movie string and returns the movie from the csv if it exists
+export async function name_to_id(movie_name : string) : Promise<MovieResult | undefined>{
+
+  return new Promise((resolve, reject) => {
+    
+    const stream = fs.createReadStream("ml-latest-small/movies.csv")
+      .pipe(csv())
+      .on("data", (row) => { 
+        const title: string = String(row.title);
+        if(title.toLowerCase().replace(/[^a-zA-Z0-9åäö]/gi, "").includes(movie_name.toLowerCase().replace(/[^a-zA-Z0-9åäö]/gi, "")) || 
+          movie_name.toLowerCase().replace(/[^a-zA-Z0-9åäö]/gi, "").includes(title.toLowerCase().replace(/[^a-zA-Z0-9åäö]/gi, ""))) { // ev. att vi hämtar en fuzzy-match lib
+            
+            const id: number = Number(row.movieId);
+            const genres: string = String(row.genres);
+            const result_movie = {movie_id : id, movie_title : title, movie_genres : genres};
+            stream.destroy();
+            resolve(result_movie);
+          }
+      })
+      .on("end", () => {
+        resolve(undefined);
+      })
+      .on("error", () => {
+        resolve(undefined);
+  });
+  });
+}
